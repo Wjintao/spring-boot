6cfcbebf48c3122446b01bba904780097ec5e3c0
wujintao <1443304718@qq.com>
2017-03-06 17:19:55 +0800

    Spring-boot demo

---
+++ spring-boot/src/main/java/com/boot/action/AppController.java
@@ -0,0 +1,13 @@
+package com.boot.action;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+@EnableAutoConfiguration  
+@RequestMapping("/test")  
+public class AppController {      
+    @RequestMapping("/sayhello")  
+    public String sayHello(){  
+        return "Hello World!";  
+    }  
+}  
